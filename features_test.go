checkpoint-restore/go-criu

commit 5b3b8adbcf3e45516ade76f05f641c5681fdcda1

Add feature checking support to go-criu
This adds the feature checking interface with the same semantics as the
feature checking through libcriu.

Signed-off-by: Adrian Reber <areber@redhat.com>

Makefile
@@ -2,6 +2,11 @@ SHELL = /bin/bash
 GO ?= go
 CC ?= gcc
 COVERAGE_PATH ?= $(shell pwd)/.coverage
+CRIU_FEATURE_MEM_TRACK = $(shell if criu check --feature mem_dirty_track > /dev/null; then echo 1; else echo 0; fi)
+CRIU_FEATURE_LAZY_PAGES = $(shell if criu check --feature uffd-noncoop > /dev/null; then echo 1; else echo 0; fi)
+CRIU_FEATURE_PIDFD_STORE = $(shell if criu check --feature pidfd_store > /dev/null; then echo 1; else echo 0; fi)
+
+export CRIU_FEATURE_MEM_TRACK CRIU_FEATURE_LAZY_PAGES CRIU_FEATURE_PIDFD_STORE
 
 all: build test phaul-test
 

features.go
@@ -1,0 +1,45 @@
+package criu
+
+import (
+	"fmt"
+
+	"github.com/checkpoint-restore/go-criu/v5/rpc"
+)
+
+// Feature checking in go-criu is based on the libcriu feature checking function.
+
+// Feature checking allows the user to check if CRIU supports
+// certain features. There are CRIU features which do not depend
+// on the version of CRIU but on kernel features or architecture.
+//
+// One example is memory tracking. Memory tracking can be disabled
+// in the kernel or there are architectures which do not support
+// it (aarch64 for example). By using the feature check a libcriu
+// user can easily query CRIU if a certain feature is available.
+//
+// The features which should be checked can be marked in the
+// structure 'struct criu_feature_check'. Each structure member
+// that is set to true will result in CRIU checking for the
+// availability of that feature in the current combination of
+// CRIU/kernel/architecture.
+//
+// Available features will be set to true when the function
+// returns successfully. Missing features will be set to false.
+
+func (c *Criu) FeatureCheck(features *rpc.CriuFeatures) (*rpc.CriuFeatures, error) {
+	resp, err := c.doSwrkWithResp(
+		rpc.CriuReqType_FEATURE_CHECK,
+		nil,
+		nil,
+		features,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.GetType() != rpc.CriuReqType_FEATURE_CHECK {
+		return nil, fmt.Errorf("Unexpected CRIU RPC response")
+	}
+
+	return features, nil
+}

main.go
@@ -87,19 +87,19 @@ func (c *Criu) sendAndRecv(reqB []byte) ([]byte, int, error) {
 }
 
 func (c *Criu) doSwrk(reqType rpc.CriuReqType, opts *rpc.CriuOpts, nfy Notify) error {
-	resp, err := c.doSwrkWithResp(reqType, opts, nfy)
+	resp, err := c.doSwrkWithResp(reqType, opts, nfy, nil)
 	if err != nil {
 		return err
 	}
 	respType := resp.GetType()
 	if respType != reqType {
-		return errors.New("unexpected responce")
+		return errors.New("unexpected CRIU RPC response")
 	}
 
 	return nil
 }
 
-func (c *Criu) doSwrkWithResp(reqType rpc.CriuReqType, opts *rpc.CriuOpts, nfy Notify) (*rpc.CriuResp, error) {
+func (c *Criu) doSwrkWithResp(reqType rpc.CriuReqType, opts *rpc.CriuOpts, nfy Notify, features *rpc.CriuFeatures) (*rpc.CriuResp, error) {
 	var resp *rpc.CriuResp
 
 	req := rpc.CriuReq{
@@ -109,6 +109,10 @@ func (c *Criu) doSwrkWithResp(reqType rpc.CriuReqType, opts *rpc.CriuOpts, nfy N
 
 	if nfy != nil {
 		opts.NotifyScripts = proto.Bool(true)
+	}
+
+	if features != nil {
+		req.Features = features
 	}
 
 	if c.swrkCmd == nil {
@@ -209,7 +213,7 @@ func (c *Criu) StartPageServer(opts *rpc.CriuOpts) error {
 
 // StartPageServerChld starts the page server and returns PID and port
 func (c *Criu) StartPageServerChld(opts *rpc.CriuOpts) (int, int, error) {
-	resp, err := c.doSwrkWithResp(rpc.CriuReqType_PAGE_SERVER_CHLD, opts, nil)
+	resp, err := c.doSwrkWithResp(rpc.CriuReqType_PAGE_SERVER_CHLD, opts, nil, nil)
 	if err != nil {
 		return 0, 0, err
 	}
@@ -220,7 +224,7 @@ func (c *Criu) StartPageServerChld(opts *rpc.CriuOpts) (int, int, error) {
 // GetCriuVersion executes the VERSION RPC call and returns the version
 // as an integer. Major * 10000 + Minor * 100 + SubLevel
 func (c *Criu) GetCriuVersion() (int, error) {
-	resp, err := c.doSwrkWithResp(rpc.CriuReqType_VERSION, nil, nil)
+	resp, err := c.doSwrkWithResp(rpc.CriuReqType_VERSION, nil, nil, nil)
 	if err != nil {
 		return 0, err
 	}

test/main.go
@@ -58,6 +58,69 @@ func doDump(c *criu.Criu, pidS string, imgDir string, pre bool, prevImg string) 
 	return nil
 }
 
+func featureCheck(c *criu.Criu) error {
+	features := &rpc.CriuFeatures{}
+	featuresToCompare := &rpc.CriuFeatures{}
+	env := os.Getenv("CRIU_FEATURE_MEM_TRACK")
+	if env != "" {
+		val, err := strconv.Atoi(env)
+		if err != nil {
+			return err
+		}
+		features.MemTrack = proto.Bool(val != 0)
+		featuresToCompare.MemTrack = proto.Bool(val != 0)
+	}
+	env = os.Getenv("CRIU_FEATURE_LAZY_PAGES")
+	if env != "" {
+		val, err := strconv.Atoi(env)
+		if err != nil {
+			return err
+		}
+		features.LazyPages = proto.Bool(val != 0)
+		featuresToCompare.LazyPages = proto.Bool(val != 0)
+	}
+	env = os.Getenv("CRIU_FEATURE_PIDFD_STORE")
+	if env != "" {
+		val, err := strconv.Atoi(env)
+		if err != nil {
+			return err
+		}
+		features.PidfdStore = proto.Bool(val != 0)
+		featuresToCompare.PidfdStore = proto.Bool(val != 0)
+	}
+
+	features, err := c.FeatureCheck(features)
+	if err != nil {
+		return err
+	}
+
+	if *features.MemTrack != *featuresToCompare.MemTrack {
+		return fmt.Errorf(
+			"Unexpected MemTrack FeatureCheck result %v:%v",
+			*features.MemTrack,
+			*featuresToCompare.MemTrack,
+		)
+	}
+
+	if *features.LazyPages != *featuresToCompare.LazyPages {
+		return fmt.Errorf(
+			"Unexpected LazyPages FeatureCheck result %v:%v",
+			*features.LazyPages,
+			*featuresToCompare.LazyPages,
+		)
+	}
+
+	if *features.PidfdStore != *featuresToCompare.PidfdStore {
+		return fmt.Errorf(
+			"Unexpected PidfdStore FeatureCheck result %v:%v",
+			*features.PidfdStore,
+			*featuresToCompare.PidfdStore,
+		)
+	}
+
+	return nil
+}
+
 // Usage: test $act $pid $images_dir
 func main() {
 	c := criu.MakeCriu()
@@ -76,6 +139,11 @@ func main() {
 	if !result {
 		log.Fatalln("CRIU version to old")
 	}
+
+	if err = featureCheck(c); err != nil {
+		log.Fatalln(err)
+	}
+
 	act := os.Args[1]
 	switch act {
 	case "dump":

commit 25e18bf48272b7e5f750004c9a78f80d2e9ff848

rpc: return checked features from CRIU
FeatureCheck returns the request object, so callers see the values they
asked CRIU to check rather than feature support reported by CRIU.

Validate and return the feature message carried by the RPC response. Add
a focused fake-RPC regression test and run root unit tests from the
standard test target.

Fixes: 5b3b8ad ("Add feature checking support to go-criu")

Assisted-by: Codex:GPT-5
Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

Makefile
@@ -13,6 +13,7 @@ build: rpc/rpc.pb.go stats/stats.pb.go
 	$(MAKE) -C crit bin/crit
 
 test: build
+	$(GO) test -v .
 	$(MAKE) -C test
 
 coverage:

features.go
@@ -41,5 +41,10 @@ func (c *Criu) FeatureCheck(features *rpc.CriuFeatures) (*rpc.CriuFeatures, erro
 		return nil, errors.New("unexpected CRIU RPC response")
 	}
 
-	return features, nil
+	checkedFeatures := resp.GetFeatures()
+	if checkedFeatures == nil {
+		return nil, errors.New("CRIU RPC response does not contain features")
+	}
+
+	return checkedFeatures, nil
 }

features_test.go
@@ -1,0 +1,99 @@
+package criu
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+	"testing"
+
+	proto "github.com/checkpoint-restore/go-criu/v8/internal/proto"
+	"github.com/checkpoint-restore/go-criu/v8/rpc"
+)
+
+func TestFeatureCheckReturnsResponseFeatures(t *testing.T) {
+	responseFeatures := &rpc.CriuFeatures{
+		MemTrack:       proto.Ptr(false),
+		MemCompression: proto.Ptr(true),
+	}
+	c, serverErr := newFeatureCheckTestClient(t, responseFeatures)
+	requestedFeatures := &rpc.CriuFeatures{
+		MemTrack:       proto.Ptr(true),
+		MemCompression: proto.Ptr(true),
+	}
+	got, featureErr := c.FeatureCheck(requestedFeatures)
+	if err := <-serverErr; err != nil {
+		t.Fatal(err)
+	}
+	if featureErr != nil {
+		t.Fatal(featureErr)
+	}
+	if !got.EqualVT(responseFeatures) {
+		t.Fatalf("FeatureCheck() = %v, want response features %v", got, responseFeatures)
+	}
+}
+
+func TestFeatureCheckRejectsMissingResponseFeatures(t *testing.T) {
+	c, serverErr := newFeatureCheckTestClient(t, nil)
+	_, featureErr := c.FeatureCheck(&rpc.CriuFeatures{MemCompression: proto.Ptr(true)})
+	if err := <-serverErr; err != nil {
+		t.Fatal(err)
+	}
+	if featureErr == nil || featureErr.Error() != "CRIU RPC response does not contain features" {
+		t.Fatalf("FeatureCheck() error = %v, want missing-features error", featureErr)
+	}
+}
+
+func newFeatureCheckTestClient(t *testing.T, responseFeatures *rpc.CriuFeatures) (*Criu, <-chan error) {
+	t.Helper()
+	fds, err := syscall.Socketpair(syscall.AF_LOCAL, syscall.SOCK_SEQPACKET, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := os.NewFile(uintptr(fds[0]), "feature-check-client")
+	server := os.NewFile(uintptr(fds[1]), "feature-check-server")
+	t.Cleanup(func() { _ = client.Close() })
+
+	serverErr := make(chan error, 1)
+	go func() {
+		defer func() { _ = server.Close() }()
+		serverErr <- serveFeatureCheck(server, responseFeatures)
+	}()
+
+	c := MakeCriu()
+	c.swrkCmd = &exec.Cmd{}
+	c.swrkSk = client
+	return c, serverErr
+}
+
+func serveFeatureCheck(server *os.File, responseFeatures *rpc.CriuFeatures) error {
+	requestBytes := make([]byte, 8192)
+	n, err := server.Read(requestBytes)
+	if err != nil {
+		return fmt.Errorf("read feature-check request: %w", err)
+	}
+	request := &rpc.CriuReq{}
+	if err := request.UnmarshalVT(requestBytes[:n]); err != nil {
+		return fmt.Errorf("unmarshal feature-check request: %w", err)
+	}
+	if request.GetType() != rpc.CriuReqType_FEATURE_CHECK {
+		return fmt.Errorf("request type = %s, want FEATURE_CHECK", request.GetType())
+	}
+	if request.GetFeatures() == nil {
+		return fmt.Errorf("feature-check request does not contain features")
+	}
+
+	response := &rpc.CriuResp{
+		Type:     proto.Ptr(rpc.CriuReqType_FEATURE_CHECK),
+		Success:  proto.Ptr(true),
+		Features: responseFeatures,
+	}
+	responseBytes, err := response.MarshalVT()
+	if err != nil {
+		return fmt.Errorf("marshal feature-check response: %w", err)
+	}
+	if _, err := server.Write(responseBytes); err != nil {
+		return fmt.Errorf("write feature-check response: %w", err)
+	}
+	return nil
+}